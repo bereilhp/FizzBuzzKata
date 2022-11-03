dcf4eab20671b5d82ab8a1e26644054dba68f436
PedroB <bereilhp@gmail.com>
2022-11-03 19:58:18 +0100

    one assert per test

--- src/test/code.test.js
+++ src/test/code.test.js
@@ -10,8 +10,6 @@ test("Check if number div by 5 and prints buzz", () => {
 
 test("Check if number is not div by 5 or 3 and print the number", () => {
     expect(x(1)).toEqual(1);
-    expect(x(7)).toEqual(7);
-    expect(x(11)).toEqual(11);
 })
 
 test("Check if number is  div by 5 and 3 and print the number", () => {
